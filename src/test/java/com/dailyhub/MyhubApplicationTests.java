42e39aa7db36dd5bf60fd495bdd4e2339944c0c5
miniSpy <774004621@qq.com>
2022-07-10 16:35:33 +0800

    dailyhub_v0.0.2.3

--- src/test/java/com/dailyhub/MyhubApplicationTests.java
+++ src/test/java/com/dailyhub/MyhubApplicationTests.java
@@ -1,19 +1,10 @@
 package com.dailyhub;
 
-import com.dailyhub.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyhubApplicationTests {
 
 
-
-    @Test
-    void contextLoads() {
-
-    }
 
 }
