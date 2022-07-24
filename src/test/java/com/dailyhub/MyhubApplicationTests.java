3e5447a202f41017f4ffa4a2c3dd94ee027a95e9
miniSpy <774004621@qq.com>
2022-07-24 16:16:18 +0800

    dailyhub_v0.0.2.3333

--- src/test/java/com/dailyhub/MyhubApplicationTests.java
+++ src/test/java/com/dailyhub/MyhubApplicationTests.java
@@ -1,9 +1,15 @@
 package com.dailyhub;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyhubApplicationTests {
+
+    @Test
+    public void test(){
+        
+    }
 
 
 
